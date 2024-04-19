2cdc528731b82b3f13e3d423eb6ed1132a931f84
Thiwain Medagama <thiwainm@gmail.com>
2024-04-20 00:43:21 +0530

    header

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
     <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-
                 <h3 class="text-light fw-bold" style="display: flex; align-items: center; gap: 10px;">
                     <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" style="width: 50px; height: auto;">
                         <ellipse cx="28" cy="29" rx="25" ry="6" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2px"></ellipse>
@@ -22,9 +21,8 @@
                         <path d="M43 51c4.31 1.28 7 3 7 5 0 3.87-9.85 7-22 7S6 59.87 6 56c0-2 2.69-3.72 7-5" fill="none" stroke="#ffffff" stroke-miterlimit="10" stroke-width="2px"></path>
                         <path d="m52.36 37.21-.72-1.87C45.67 37.67 37.06 39 28 39s-17.67-1.33-23.64-3.66l-.72 1.87.68.25-.68 1.75C9.82 41.62 18.7 43 28 43s18.18-1.38 24.36-3.79l-.68-1.75Z" fill="#ffffff"></path>
                         <path d="M30 1c0 4.5-4 4.5-4 9s4 4.5 4 9M11 5c0 4-4 4-4 8s4 4 4 8M47 5c0 4-4 4-4 8s4 4 4 8M39 4l1-1M36 4l-1-1M36 7l-1 1M39 7l1 1M60 19l1-1M57 19l-1-1M57 22l-1 1M60 22l1 1M19 14l1-1M16 14l-1-1M16 17l-1 1M19 17l1 1" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2px"></path>
-                    </svg>    Teapot
+                    </svg><span></span>Teapot
                 </h3>
-
             </a>
             <!-- Toggler for mobile view -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
