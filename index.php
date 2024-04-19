fd206419b4a580c405a68427f2eebca98c388f56
Thiwain Medagama <thiwainm@gmail.com>
2024-04-19 23:23:10 +0530

    1st-commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teapot | Home</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+
+
+
+    <script src="js/script.js"></script>
+    <script src="js/ajax.js"></script>
+</body>
+
+</html>
